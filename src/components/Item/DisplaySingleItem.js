2573aee0ceb13e3fa5e14308935950319e2d388a
John Roberts <bigjlr2004@yahoo.com>
2023-02-17 10:31:19 -0600

    Added Main page styling.

--- src/components/Item/DisplaySingleItem.js
+++ src/components/Item/DisplaySingleItem.js
@@ -1,9 +1,11 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 import { fetchDelete, returnDate } from "../../Api_Manager"
 import { HomeScreenComment } from "../Comments/HomeScreenComment"
 
 export const DisplaySingleItem = ({ filteredItems, getAllItems }) => {
-
-
+    const [commentAdd, setCommentAdd] = useState("false")
+    const navigate = useNavigate()
     const handleChangeStatus = (event, obj) => {
         event.preventDefault()
         const copy = { ...obj }
@@ -16,7 +18,6 @@ export const DisplaySingleItem = ({ filteredItems, getAllItems }) => {
                 "Content-Type": "application/json"
             },
             method: "PATCH",
-
             // Sending only the fields that need to be updated
             body: JSON.stringify({
                 status: `${copy.status}`
@@ -34,22 +35,57 @@ export const DisplaySingleItem = ({ filteredItems, getAllItems }) => {
     return (<>
         {filteredItems.map((itemObj) => {
             return (
-                <div className="card " key={itemObj.id}>
+                <div className="track-card" key={itemObj.id}>
+                    <header className="card__header">
+                        <div>Category: {itemObj.category.name}</div>
+                        <div className={`${itemObj.status === "active" ? "status-active" : "status-inactive"}`}></div>
+                    </header>
+                    <div className="track-card-container">
+                        <div>
+                            <div className="track-card-info">
+                                <h5 className="track-card-name">{itemObj.name.toUpperCase()}</h5>
+                                Reason: {itemObj.description}
+                                <div>Started: {returnDate(itemObj.dateAdded)}</div>
+                            </div>
+                            <div>
+                                <HomeScreenComment itemObj={itemObj}
+                                    commentAdd={commentAdd}
+                                    setCommentAdd={setCommentAdd}
+                                />
+                            </div>
+                        </div>
+                        <div className="item__card__buttons">
+                            <span className={`${commentAdd === "true" ? "invisible" : "visible"}`}>
+                                <button
+                                    id={itemObj.id}
+                                    onClick={(event) => {
+                                        handleDeleteItem(event)
+                                    }}>
+                                    Delete Item
+                                </button>
 
-                    <div className={`${itemObj.status === "active" ? "card-header status-active" : " status-inactive card-header "}`}>
-                        <div className="card-title"> First Steps: {returnDate(itemObj.dateAdded)}</div><div>{itemObj?.category?.name.toUpperCase()}</div>
-                    </div>
-                    <div className="card-body">
+                            </span>
+                            <span className={`${itemObj.status === "inactive" || commentAdd === "true" ? "invisible" : "visible"}`}>
+                                <button
+                                    id={itemObj.id}
+                                    onClick={(event) => {
+                                        navigate(`/items/${itemObj.id}/edit`)
+                                    }}>
+                                    Edit Item
+                                </button>
+                                <button
+                                    id={itemObj.id}
+                                    value={"inactive"}
+                                    onClick={(event) => {
+                                        handleChangeStatus(event, itemObj)
+                                    }}>
+                                    Retire Item
+                                </button>
+                            </span>
 
-                        <h5 className="card-title">{itemObj.name.toUpperCase()}</h5>
-                        <h6 className="card-text">Reason: {itemObj.description}</h6>
+                        </div>
                     </div>
-                    <HomeScreenComment itemObj={itemObj}
-                        handleChangeStatus={handleChangeStatus}
-                        handleDeleteItem={handleDeleteItem} />
                 </div>
-
-
             )
         })}
     </>)
